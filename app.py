2fa13200199064d3ea442b827a47321674dba4e5
rob <caplarobko@gmail.com>
2023-01-22 20:07:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sslify import SSLify
+
 from routes.api import api
 
 app = Flask(__name__)
+sslify = SSLify(app)
 app.register_blueprint(api)
 
 if __name__ == '__main__':
